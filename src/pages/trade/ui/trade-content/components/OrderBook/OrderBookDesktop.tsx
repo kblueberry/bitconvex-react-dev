9e0337a01ed19df42b5daddfb3f32c7fae6fed51
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-04-24 17:16:45 +0300

    component for orderBook desktop

--- src/pages/trade/ui/trade-content/components/OrderBook/OrderBookDesktop.tsx
+++ src/pages/trade/ui/trade-content/components/OrderBook/OrderBookDesktop.tsx
@@ -1,17 +1,12 @@
-import { Stack, Table, Text } from "@mantine/core";
+import { Table } from "@mantine/core";
 import clsx from "clsx";
-import { useEffect, useState } from "react";
 
 import { NegativeTrendIcon } from "@/shared/ui/icon/NegativeTrendIcon";
 import { PositiveTrendIcon } from "@/shared/ui/icon/PositiveTrendIcon";
 import { SortIcon } from "@/shared/ui/icon/SortIcon";
 
-import { Container } from "../Container/Container";
 import classes from "./OrderBook.module.css";
-import { OrderBookDesktop } from "./OrderBookDesktop";
-import { OrderBookMobile } from "./OrderBookMobile";
 
-const categories = ["All", "Asks", "Bids"] as const;
 const header = ["Price USD", "Qty BTC", "Total USD"];
 const rows = [
   {
@@ -139,29 +134,67 @@ const fullRows = [
   },
 ];
 
-export const OrderBook = () => {
-  const [activeCategory, setActiveCategory] = useState<(typeof categories)[number]>(categories[0]);
-  const [isMobile, setIsMobile] = useState<boolean>(false);
-
-  useEffect(() => setIsMobile(window.innerWidth < 600), []);
-
+export const OrderBookDesktop = ({ activeCategory }: { activeCategory: string }) => {
   return (
-    <Container style={{ height: "100%" }}>
-      <Stack gap={32} h={"100%"}>
-        <Text className={classes.orderBookTitle}>Order Book</Text>
-        <div className={classes.orderBookButtonsWrapper}>
-          {categories.map((cat) => (
-            <button
-              key={cat}
-              className={clsx(classes.orderBooksButton, { [classes.active]: activeCategory === cat })}
-              onClick={() => setActiveCategory(cat)}
-            >
-              {cat}
-            </button>
+    <Table>
+      <Table.Thead className={classes.tableTHead}>
+        <Table.Tr>
+          {header.map((head) => (
+            <Table.Th key={head} className={classes.tableTh}>
+              <div>
+                <p>{head}</p>
+                <SortIcon />
+              </div>
+            </Table.Th>
           ))}
-        </div>
-        {isMobile ? <OrderBookMobile {...{ activeCategory }} /> : <OrderBookDesktop {...{ activeCategory }} />}
-      </Stack>
-    </Container>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody className={classes.tableBody}>
+        {(activeCategory === "All" ? rows : fullRows).map((row) => (
+          <Table.Tr
+            style={{
+              background: `linear-gradient(90deg, rgba(12,13,16,1) ${100 - row.fill}%, ${
+                activeCategory === "All" || activeCategory === "Bids" ? "rgba(90,222,167,0.8)" : "rgba(244, 74, 74, 0.8)"
+              } ${100 - row.fill}%)`,
+            }}
+            key={row.id}
+            className={activeCategory === "All" || activeCategory === "Bids" ? classes.positive : classes.negative}
+          >
+            {row.cells.map((td) => (
+              <Table.Td key={td}>
+                <p>{td}</p>
+              </Table.Td>
+            ))}
+          </Table.Tr>
+        ))}
+        <Table.Tr>
+          <Table.Td className={classes.totalCell} colSpan={100}>
+            <p
+              className={clsx(classes.orderBookInfo, {
+                [classes.negative]: activeCategory === "Bids",
+                [classes.positive]: activeCategory === "All" || activeCategory === "Asks",
+              })}
+            >
+              $ 38,555.19 {activeCategory === "Bids" ? <NegativeTrendIcon /> : <PositiveTrendIcon />}
+            </p>
+          </Table.Td>
+        </Table.Tr>
+        {activeCategory === "All" &&
+          rows.map((row) => (
+            <Table.Tr
+              style={{
+                background: `linear-gradient(90deg, rgba(12,13,16,1) ${100 - row.fill}%, rgba(244, 74, 74, 0.8)
+          ${100 - row.fill}%)`,
+              }}
+              key={row.id}
+              className={classes.negative}
+            >
+              {row.cells.map((td) => (
+                <Table.Td key={td}>{td}</Table.Td>
+              ))}
+            </Table.Tr>
+          ))}
+      </Table.Tbody>
+    </Table>
   );
 };
