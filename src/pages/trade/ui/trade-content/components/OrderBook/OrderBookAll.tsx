c6dd3f60d5b8e057a3feb593b0f577fac2e212b2
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-04-24 18:25:13 +0300

    trade content - orders list - show medium order price

--- src/pages/trade/ui/trade-content/components/OrderBook/OrderBookAll.tsx
+++ src/pages/trade/ui/trade-content/components/OrderBook/OrderBookAll.tsx
@@ -1,13 +1,20 @@
+import { Stack, Text } from "@mantine/core";
 import clsx from "clsx";
 
+import { Container } from "../Container/Container";
 import classes from "./OrderBook.module.css";
 import { StocksTable } from "./StocksTable/StocksTable";
 
 export const OrderBookAll = ({ header, rows }: { header: Array<string>; rows: Array<any> }) => {
   return (
-    <div className={clsx(classes.flexSpaceBetween, classes.columnDirected)}>
-      <StocksTable header={header} rows={rows} isPositive={true} />
-      <StocksTable header={header} rows={rows} isPositive={false} />
-    </div>
+    <Stack>
+      <Container>
+        <div className={clsx(classes.flexSpaceBetween, classes.columnDirected)}>
+          <StocksTable header={header} rows={rows} isPositive={true} />
+          <Text className={classes.tradeOrderBookInfo}>38,555.19</Text>
+          <StocksTable header={header} rows={rows} isPositive={false} />
+        </div>
+      </Container>
+    </Stack>
   );
 };
--- src/pages/trade/ui/trade-content/components/Payment/Payment.tsx
+++ src/pages/trade/ui/trade-content/components/Payment/Payment.tsx
@@ -1,8 +1,11 @@
-import { Stack } from "@mantine/core";
+import { Group, Stack, Text } from "@mantine/core";
 
 import { Tabs } from "@/shared/ui/tabs";
 
+import { Coins } from "../Coins/Coins";
 import { Container } from "../Container/Container";
+import containerClasses from "../Container/Container.module.css";
+import classes from "../TradeChart/TradeChart.module.css";
 import { BuyTab } from "./components/tabs/BuyTab/BuyTab";
 import { SellTab } from "./components/tabs/SellTab/SellTab";
 
@@ -13,8 +16,19 @@ const tabs = [
 export const Payment = () => {
   return (
     <Stack>
-      <Container>
-        <Tabs overflowContainer={false} tabs={tabs} />
+      <Container className={containerClasses.payment}>
+        {window.innerWidth < 600 && (
+          <>
+            <Group gap={8} className={classes.tradeChartWrapper}>
+              <Text className={classes.tradeChartTitle}>Bitcoin/USDT Chart</Text>
+              <Coins />
+            </Group>
+          </>
+        )}
+
+        <Group mt={20} mb={32}>
+          <Tabs overflowContainer={false} tabs={tabs} />
+        </Group>
       </Container>
     </Stack>
   );
