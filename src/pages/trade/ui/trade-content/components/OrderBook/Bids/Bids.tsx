c4c32008e2f30afc7a020826bf4fbb5fefa1baa5
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-04-25 12:02:02 +0300

    refactor background colors of asks and bids on All tab of Order Book

--- src/pages/trade/ui/trade-content/components/OrderBook/Bids/Bids.tsx
+++ src/pages/trade/ui/trade-content/components/OrderBook/Bids/Bids.tsx
@@ -15,18 +15,17 @@ export const Bids = ({ rows, isPositive, activeCategory }: BidsProps) => {
     <Table.Tbody className={classes.tableBody}>
       {rows.map((row) => (
         <Table.Tr
-          style={{
-            background: `linear-gradient(90deg, rgba(12,13,16,1) ${100 - row.fill}%, ${
-              isPositive ? "rgba(90,222,167,0.8)" : "rgba(244, 74, 74, 0.8)"
-            } ${100 - row.fill}%)`,
-          }}
           key={row.id}
           className={
             isPositive ? classes.positive : clsx(classes.negative, { [classes.orderRowReversed]: !!activeCategory && activeCategory === "All" })
           }
         >
           {row.cells.map((td, index) => (
-            <Table.Td key={td} hidden={index === 2}>
+            <Table.Td
+              key={td}
+              hidden={index === 2}
+              className={clsx(classes.tableCell, isPositive ? classes.tableCellPositive : classes.tableCellNegative)}
+            >
               <p>{td}</p>
             </Table.Td>
           ))}
