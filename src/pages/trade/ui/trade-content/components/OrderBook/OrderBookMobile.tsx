e699fe50616be65d1d1fdaba965a8e03bbbf715f
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-04-25 10:54:31 +0300

    common constants for order book components in separate constants file

--- src/pages/trade/ui/trade-content/components/OrderBook/OrderBookMobile.tsx
+++ src/pages/trade/ui/trade-content/components/OrderBook/OrderBookMobile.tsx
@@ -1,67 +1,9 @@
 import clsx from "clsx";
 
+import { header, rows } from "./OrderBook.constants";
 import classes from "./OrderBook.module.css";
 import { OrderBookAll } from "./OrderBookAll";
 import { StocksTable } from "./StocksTable/StocksTable";
-
-const header = ["Price USD", "Qty BTC"];
-const rows = [
-  {
-    id: 1,
-    fill: 60,
-    cells: ["$ 38,555.19", "0.299993"],
-  },
-  {
-    id: 2,
-    fill: 10,
-    cells: ["$ 38,555.19", "0.299993"],
-  },
-  {
-    id: 3,
-    fill: 0,
-    cells: ["$ 38,555.19", "0.299993"],
-  },
-  {
-    id: 4,
-    fill: 50,
-    cells: ["$ 38,555.19", "0.299993"],
-  },
-  {
-    id: 5,
-    fill: 50,
-    cells: ["$ 38,555.19", "0.299993"],
-  },
-  {
-    id: 6,
-    fill: 80,
-    cells: ["$ 38,555.19", "0.299993"],
-  },
-  {
-    id: 7,
-    fill: 20,
-    cells: ["$ 38,555.19", "0.299993"],
-  },
-  {
-    id: 8,
-    fill: 0,
-    cells: ["$ 38,555.19", "0.299993"],
-  },
-  {
-    id: 9,
-    fill: 20,
-    cells: ["$ 38,555.19", "0.299993"],
-  },
-  {
-    id: 10,
-    fill: 95,
-    cells: ["$ 38,555.19", "0.299993"],
-  },
-  {
-    id: 11,
-    fill: 45,
-    cells: ["$ 38,555.19", "0.299993"],
-  },
-];
 
 export const OrderBookMobile = ({ activeTab, activeCategory }: { activeTab: string; activeCategory: string }) => {
   return activeTab === "Chart" ? (
