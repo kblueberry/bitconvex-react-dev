a8f87193e80ffb058c6e6013b0886c305a0e9459
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-04-29 17:12:06 +0300

    add horizontal scroll on trade history  tables

--- src/pages/trade/ui/trade-content/components/TradeHistory/tabs/HistoryTab/HistoryTabMobile.tsx
+++ src/pages/trade/ui/trade-content/components/TradeHistory/tabs/HistoryTab/HistoryTabMobile.tsx
@@ -4,155 +4,10 @@ import { useState } from "react";
 
 import { MarketSortIcon } from "@/shared/ui";
 
+import tradeHistoryClasses from "../../TradeHistory.module.css";
+import { dataMobile, headerMobile } from "./HistoryTab.constants";
 import classes from "./HistoryTab.module.css";
 
-const header = ["Pairs", "Direction", "Filled Qty", "Filled Price"];
-const data = [
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Filled Qty",
-      value: "163.0368 BTC",
-    },
-    {
-      key: "Filled Price",
-      value: "45550.27",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Buy",
-    },
-    {
-      key: "Filled Qty",
-      value: "163.0368 BTC",
-    },
-    {
-      key: "Filled Price",
-      value: "45550.27",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Filled Qty",
-      value: "163.0368 BTC",
-    },
-    {
-      key: "Filled Price",
-      value: "45550.27",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Buy",
-    },
-    {
-      key: "Filled Qty",
-      value: "163.0368 BTC",
-    },
-    {
-      key: "Filled Price",
-      value: "45550.27",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Filled Qty",
-      value: "163.0368 BTC",
-    },
-    {
-      key: "Filled Price",
-      value: "45550.27",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Filled Qty",
-      value: "163.0368 BTC",
-    },
-    {
-      key: "Filled Price",
-      value: "45550.27",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Filled Qty",
-      value: "163.0368 BTC",
-    },
-    {
-      key: "Filled Price",
-      value: "45550.27",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Filled Qty",
-      value: "163.0368 BTC",
-    },
-    {
-      key: "Filled Price",
-      value: "45550.27",
-    },
-  ],
-];
 export const HistoryTabMobile = () => {
   const [sortState, setSortState] = useState<{ sortCol: string; sortFunc: 1 | 2 | 3 }>({ sortCol: "", sortFunc: 1 });
   const sortHandler = (cell: string) => {
@@ -160,38 +15,40 @@ export const HistoryTabMobile = () => {
     if (cell === sortState.sortCol) setSortState({ ...sortState, sortFunc: sortState.sortFunc === 3 ? 1 : ((sortState.sortFunc + 1) as 2 | 3) });
   };
   return (
-    <Table className={classes.table} withRowBorders={false}>
-      <Table.Thead>
-        <Table.Tr>
-          {header.map((cell) => (
-            <Table.Th key={cell}>
-              <Group gap={0} onClick={() => sortHandler(cell)}>
-                {cell}
-                <div
-                  className={clsx(
-                    classes.sortArrowWrapper,
-                    sortState.sortCol === cell && (sortState.sortFunc === 2 || sortState.sortFunc === 3) && classes.active,
-                    sortState.sortCol === cell && sortState.sortFunc === 3 && classes.rotate,
-                  )}
-                >
-                  <MarketSortIcon width={20} height={20} />
-                </div>
-              </Group>
-            </Table.Th>
-          ))}
-        </Table.Tr>
-      </Table.Thead>
-      <Table.Tbody>
-        {data.map((row, i) => (
-          <Table.Tr key={i}>
-            {row.map((cell) => (
-              <Table.Td key={cell.key} className={clsx({ [classes.green]: cell.value === "Buy", [classes.red]: cell.value === "Sell" })}>
-                {cell.value}
-              </Table.Td>
+    <div className={tradeHistoryClasses.containerScrolled}>
+      <Table className={classes.table} withRowBorders={false}>
+        <Table.Thead>
+          <Table.Tr>
+            {headerMobile.map((cell) => (
+              <Table.Th key={cell}>
+                <Group gap={0} onClick={() => sortHandler(cell)}>
+                  {cell}
+                  <div
+                    className={clsx(
+                      classes.sortArrowWrapper,
+                      sortState.sortCol === cell && (sortState.sortFunc === 2 || sortState.sortFunc === 3) && classes.active,
+                      sortState.sortCol === cell && sortState.sortFunc === 3 && classes.rotate,
+                    )}
+                  >
+                    <MarketSortIcon width={20} height={20} />
+                  </div>
+                </Group>
+              </Table.Th>
             ))}
           </Table.Tr>
-        ))}
-      </Table.Tbody>
-    </Table>
+        </Table.Thead>
+        <Table.Tbody>
+          {dataMobile.map((row, i) => (
+            <Table.Tr key={i}>
+              {row.map((cell) => (
+                <Table.Td key={cell.key} className={clsx({ [classes.green]: cell.value === "Buy", [classes.red]: cell.value === "Sell" })}>
+                  {cell.value}
+                </Table.Td>
+              ))}
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+    </div>
   );
 };
--- src/pages/trade/ui/trade-content/components/TradeHistory/tabs/OrdersTab/OrdersTabMobile.tsx
+++ src/pages/trade/ui/trade-content/components/TradeHistory/tabs/OrdersTab/OrdersTabMobile.tsx
@@ -4,155 +4,10 @@ import { useState } from "react";
 
 import { MarketSortIcon } from "@/shared/ui";
 
+import tradeHistoryClasses from "../../TradeHistory.module.css";
+import { dataMobile, headerMobile } from "./OrdersTab.constants";
 import classes from "./OrdersTab.module.css";
 
-const header = ["Pairs", "Direction", "Order Type", "Qty", "Action"];
-const data = [
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Order Type",
-      value: "Limit",
-    },
-    {
-      key: "Qty",
-      value: "1583.4739 BTC",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Buy",
-    },
-    {
-      key: "Order Type",
-      value: "Limit",
-    },
-    {
-      key: "Qty",
-      value: "1583.4739 BTC",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Order Type",
-      value: "Limit",
-    },
-    {
-      key: "Qty",
-      value: "1583.4739 BTC",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Order Type",
-      value: "Limit",
-    },
-    {
-      key: "Qty",
-      value: "1583.4739 BTC",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Order Type",
-      value: "Limit",
-    },
-    {
-      key: "Qty",
-      value: "1583.4739 BTC",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Order Type",
-      value: "Limit",
-    },
-    {
-      key: "Qty",
-      value: "1583.4739 BTC",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Order Type",
-      value: "Limit",
-    },
-    {
-      key: "Qty",
-      value: "1583.4739 BTC",
-    },
-  ],
-  [
-    {
-      key: "Pairs",
-      value: "Bitcoin/USDT",
-    },
-    {
-      key: "Direction",
-      value: "Sell",
-    },
-    {
-      key: "Order Type",
-      value: "Limit",
-    },
-    {
-      key: "Qty",
-      value: "1583.4739 BTC",
-    },
-  ],
-];
 export const OrdersTabMobile = () => {
   const [sortState, setSortState] = useState<{ sortCol: string; sortFunc: 1 | 2 | 3 }>({ sortCol: "", sortFunc: 1 });
   const sortHandler = (cell: string) => {
@@ -160,43 +15,45 @@ export const OrdersTabMobile = () => {
     if (cell === sortState.sortCol) setSortState({ ...sortState, sortFunc: sortState.sortFunc === 3 ? 1 : ((sortState.sortFunc + 1) as 2 | 3) });
   };
   return (
-    <Table className={classes.table} withRowBorders={false}>
-      <Table.Thead>
-        <Table.Tr>
-          {header.map((cell) => (
-            <Table.Th key={cell}>
-              <Group gap={0} onClick={() => sortHandler(cell)}>
-                {cell}
-                <div
-                  className={clsx(
-                    classes.sortArrowWrapper,
-                    sortState.sortCol === cell && (sortState.sortFunc === 2 || sortState.sortFunc === 3) && classes.active,
-                    sortState.sortCol === cell && sortState.sortFunc === 3 && classes.rotate,
-                  )}
-                >
-                  <MarketSortIcon width={20} height={20} />
-                </div>
-              </Group>
-            </Table.Th>
-          ))}
-        </Table.Tr>
-      </Table.Thead>
-      <Table.Tbody>
-        {data.map((row, i) => (
-          <Table.Tr key={i}>
-            {row.map((cell) => (
-              <Table.Td key={cell.key} className={clsx({ [classes.green]: cell.value === "Buy", [classes.red]: cell.value === "Sell" })}>
-                {cell.value}
-              </Table.Td>
+    <div className={tradeHistoryClasses.containerScrolled}>
+      <Table className={classes.table} withRowBorders={false}>
+        <Table.Thead>
+          <Table.Tr>
+            {headerMobile.map((cell) => (
+              <Table.Th key={cell}>
+                <Group gap={0} onClick={() => sortHandler(cell)}>
+                  {cell}
+                  <div
+                    className={clsx(
+                      classes.sortArrowWrapper,
+                      sortState.sortCol === cell && (sortState.sortFunc === 2 || sortState.sortFunc === 3) && classes.active,
+                      sortState.sortCol === cell && sortState.sortFunc === 3 && classes.rotate,
+                    )}
+                  >
+                    <MarketSortIcon width={20} height={20} />
+                  </div>
+                </Group>
+              </Table.Th>
             ))}
-            <Table.Td>
-              <button>
-                <CloseButton className={classes.closeButton} />
-              </button>
-            </Table.Td>
           </Table.Tr>
-        ))}
-      </Table.Tbody>
-    </Table>
+        </Table.Thead>
+        <Table.Tbody>
+          {dataMobile.map((row, i) => (
+            <Table.Tr key={i}>
+              {row.map((cell) => (
+                <Table.Td key={cell.key} className={clsx({ [classes.green]: cell.value === "Buy", [classes.red]: cell.value === "Sell" })}>
+                  {cell.value}
+                </Table.Td>
+              ))}
+              <Table.Td>
+                <button>
+                  <CloseButton className={classes.closeButton} />
+                </button>
+              </Table.Td>
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+    </div>
   );
 };
