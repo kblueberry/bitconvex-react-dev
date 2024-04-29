e25862ca84445dbbc2aa0cab6633d70b192ede4f
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-04-29 12:03:36 +0300

    temporarily removed broken icons from trade page markup

--- src/pages/trade/page.tsx
+++ src/pages/trade/page.tsx
@@ -1,19 +1,18 @@
-import { Image } from "@mantine/core";
 import "react-circular-progressbar/dist/styles.css";
 
 import { Container, Footer, Header, Wrapper } from "@/shared/ui";
 
 import containerClasses from "../../shared/ui/container/styles.module.css";
-import classes from "./styles.module.css";
 import { TradeContent } from "./ui";
 
 export function Page() {
   return (
     <Wrapper>
-      <Image draggable={false} src={`${import.meta.env.BASE_URL}assets/light/main/1.png`} alt="main-light-1" className={classes.lightOne} />
+      {/* TODO figure out why these icons needed */}
+      {/* <Image draggable={false} src={`${import.meta.env.BASE_URL}assets/light/main/1.png`} alt="main-light-1" className={classes.lightOne} />
       <Image draggable={false} src={`${import.meta.env.BASE_URL}assets/light/my-profile/2.png`} alt="main-light-2" className={classes.lightTwo} />
       <Image draggable={false} src={`${import.meta.env.BASE_URL}assets/light/my-profile/3.png`} alt="main-light-3" className={classes.lightThree} />
-      <Image draggable={false} src={`${import.meta.env.BASE_URL}assets/light/my-profile/1.png`} alt="light-3" className={classes.lightFour} />
+      <Image draggable={false} src={`${import.meta.env.BASE_URL}assets/light/my-profile/1.png`} alt="light-3" className={classes.lightFour} /> */}
 
       <Header />
       <Container width={1460} className={containerClasses.highOverlayPriority}>
